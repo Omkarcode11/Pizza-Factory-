ce8d812d6e494a164ae8f1063e56b0987646e68b
= <=>
2025-05-30 23:49:10 +0530

    Add order model with schema for pizza ordering system

---
+++ src/models/order.model.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  pizza_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Pizza",
+    required: true,
+  },
+  delivery_agent_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  products: [
+    {
+      product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Pizza",
+        required: true,
+      },
+    },
+  ],
+  chef: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    default: null,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: [
+      "PENDING",
+      "ACCEPTED",
+      "PREPARING",
+      "OUT_FOR_DELIVERY",
+      "DELIVERED",
+      "CANCELLED",
+    ],
+    default: "PENDING",
+  },
+},{timestamps: true, versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true }}); 
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
+// This code defines a Mongoose schema for an Order model in a pizza ordering system.

afa7f848f1192ad658ac9385944d750cec576b03
= <=>
2025-06-08 16:00:12 +0530

    Refactor Payment model schema to use enums for payment method and status, and improve code structure

--- src/models/payment.model.ts
+++ src/models/payment.model.ts
@@ -1,25 +1,50 @@
 import mongoose from "mongoose";
+import { PaymentMode, PaymentStatus } from "../service/payment/types";
 
-const paymentSchema = new mongoose.Schema({
-  order_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Order",
-    require: true,
+const paymentSchema = new mongoose.Schema(
+  {
+    order_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+    payment_method: {
+      type: String,
+      enum: PaymentMode,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: PaymentStatus,
+      default: PaymentStatus.PENDING,
+      required: true,
+    },
+    transaction_id: {
+      type: String,
+      unique: true,
+      default: `TNX-${Date.now()}`
+    },
+    paid_at: {
+      type: Date,
+    },
+    currency: {
+      type: String,
+      default: "INR",
+    },
   },
-  payment_method: {
-    type: String,
-    enum: ["CASH", "CARD", "ONLINE"],
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  status: {
-    type: String,
-    enum: ["PENDING", "COMPLETED", "FAILED"],
-    default: "PENDING",
-    required: true,
-  },
-},{timestamps: true, versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true }});
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Payment = mongoose.model("payment", paymentSchema);
+
+export default Payment;
